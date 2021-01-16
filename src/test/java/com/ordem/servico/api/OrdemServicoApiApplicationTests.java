7c01ebd3be2b229e2124deab0d0640388e7fd232
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2021-01-16 19:02:44 -0300

    first commit

---
+++ src/test/java/com/ordem/servico/api/OrdemServicoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ordem.servico.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdemServicoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
